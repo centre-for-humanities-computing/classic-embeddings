5f7323c750749ec7511111803cefb467c8491540
Márton Kardos <power.up1163@gmail.com>
2023-04-17 13:31:02 +0200

    Added biblical font

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from dash_extensions.enrich import dash
+from dash_extensions.enrich import Dash
 from embedding_explorer.app import get_dash_app
 from embedding_explorer.blueprints.dashboard import create_dashboard
 from embedding_explorer.model import Model
@@ -16,12 +16,28 @@ def get_models(path: str) -> dict[str, Model]:
         model_path = Path(path).joinpath(model_name, "model.gensim")
         keyed_vectors = KeyedVectors.load(str(model_path))
         models[model_name] = Model.from_keyed_vectors(keyed_vectors)
+        break
     return models
 
 
 models = get_models(path="dat")
 blueprint, register_pages = create_dashboard(models, fuzzy_search=True)
-app = get_dash_app(blueprint, use_pages=True)
+app = Dash(
+    blueprint=blueprint,
+    use_pages=True,
+    name=__name__,
+    assets_folder="assets/",
+    pages_folder="",
+    external_scripts=[
+        {
+            "src": "https://cdn.tailwindcss.com",
+        },
+        {
+            "src": "https://kit.fontawesome.com/9640e5cd85.js",
+            "crossorigin": "anonymous",
+        },
+    ],
+)
 register_pages(app)
 
 server = app.server
