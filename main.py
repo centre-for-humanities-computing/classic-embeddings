c83f3f715c0ac9c82e333eb5cb9b34ae21776edf
Márton Kardos <power.up1163@gmail.com>
2023-04-17 13:53:12 +0200

    Adjusted to new version of embedding_explorer

--- main.py
+++ main.py
@@ -16,29 +16,15 @@ def get_models(path: str) -> dict[str, Model]:
         model_path = Path(path).joinpath(model_name, "model.gensim")
         keyed_vectors = KeyedVectors.load(str(model_path))
         models[model_name] = Model.from_keyed_vectors(keyed_vectors)
-        break
     return models
 
 
 models = get_models(path="dat")
 blueprint, register_pages = create_dashboard(models, fuzzy_search=True)
-app = Dash(
-    blueprint=blueprint,
-    use_pages=True,
-    name=__name__,
-    assets_folder="assets/",
-    pages_folder="",
-    external_scripts=[
-        {
-            "src": "https://cdn.tailwindcss.com",
-        },
-        {
-            "src": "https://kit.fontawesome.com/9640e5cd85.js",
-            "crossorigin": "anonymous",
-        },
-    ],
+app = get_dash_app(
+    blueprint=blueprint, name=__name__, use_pages=True, assets_folder="assets/"
 )
-register_pages(app)
+register_pages()
 
 server = app.server
 
